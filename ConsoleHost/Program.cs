06c09124c9704a7be70891e77a7f993d523338b4
konstantin <konstantin@luum.com>
2024-02-12 14:09:18 -0500

    Wrote README.md

--- ConsoleHost/Program.cs
+++ ConsoleHost/Program.cs
@@ -4,7 +4,7 @@ using SqlSchema;
 using SqlSchema.Options;
 
 [assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyTitle("Sql Schema Helper")]
+[assembly: AssemblyTitle("Sql Schema Comparer")]
 [assembly: AssemblyCopyright("Copyright (c) 2024 Konstantin Surkov")]
 
 Parser.Default.ParseArguments<Extract, Compare>(args)
